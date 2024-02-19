8ca9023dc1e1ff8be5adec49874d750c194059aa
angela <adeng32@uwo.ca>
2024-02-19 12:22:00 -0500

    Completed tag implementation.

--- personal-website-v2/src/components/Tag.js
+++ personal-website-v2/src/components/Tag.js
@@ -6,22 +6,93 @@ import useMousePosition from "../useMousePosition";
 function Tag() {
   const { tag } = useContext(TagContext);
   const mousePosition = useMousePosition();
+  const [prevPosition, setPrevPosition] = useState({
+    x: 0,
+    y: 0,
+  });
+
   const [dynamicStyles, setDynamicStyles] = useState({
     '--x': '0px',
     '--y': '0px',
+    '--oldx': '0px',
+    '--oldy': '0px',
   });
 
+  // update oldx & oldy + 2px
   useEffect(() => {
-    setDynamicStyles({
-      '--x': `${mousePosition.x}px`,
-      '--y': `${mousePosition.y}px`,
-    });
-  }, [mousePosition]);
+    const intervalID = setInterval(() => {
+      if(prevPosition.x > mousePosition.x && prevPosition.y > mousePosition.y){
+        setPrevPosition({
+            x: prevPosition.x - 2,
+            y: prevPosition.y - 2,
+          });
+      }
 
+      if(prevPosition.x < mousePosition.x && prevPosition.y < mousePosition.y){
+        setPrevPosition({
+            x: prevPosition.x + 2,
+            y: prevPosition.y + 2,
+          });
+      }
+
+      if(prevPosition.x > mousePosition.x && prevPosition.y < mousePosition.y){
+        setPrevPosition({
+            x: prevPosition.x - 2,
+            y: prevPosition.y + 2,
+          });
+      }
+
+      if(prevPosition.x < mousePosition.x && prevPosition.y > mousePosition.y){
+        setPrevPosition({
+            x: prevPosition.x + 2,
+            y: prevPosition.y - 2,
+          });
+      }
+
+      // equal cases
+
+      if(prevPosition.x === mousePosition.x && prevPosition.y > mousePosition.y){
+        setPrevPosition({
+            x: prevPosition.x,
+            y: prevPosition.y - 2,
+          });
+      }
+
+      if(prevPosition.x === mousePosition.x && prevPosition.y < mousePosition.y){
+        setPrevPosition({
+            x: prevPosition.x,
+            y: prevPosition.y + 2,
+          });
+      }
+
+      if(prevPosition.x > mousePosition.x && prevPosition.y === mousePosition.y){
+        setPrevPosition({
+            x: prevPosition.x - 2,
+            y: prevPosition.y,
+          });
+      }
+
+      if(prevPosition.x < mousePosition.x && prevPosition.y === mousePosition.y){
+        setPrevPosition({
+            x: prevPosition.x + 2,
+            y: prevPosition.y,
+          });
+      }
+
+      setDynamicStyles({
+        '--oldx': `${prevPosition.x}px`,
+        '--oldy': `${prevPosition.y}px`,
+        '--x': `${mousePosition.x}px`,
+        '--y': `${mousePosition.y}px`,
+      });
+    }, 1);
+
+    return () => clearInterval(intervalID);
+  }, [prevPosition, mousePosition]);
+  
   return (
     <div id="chase" style={dynamicStyles}>
       <p>≽^•ܫ•^≼</p>
-      {JSON.stringify(mousePosition)}
     </div>
   );
 }
