8e517dfee15ef8c2818cd2214291ecbe45134384
angela <adeng32@uwo.ca>
2024-02-22 22:40:43 -0500

    Added Disc functions & Audio.

--- personal-website-v2/src/sub-components/Disc.js
+++ personal-website-v2/src/sub-components/Disc.js
@@ -1,25 +1,18 @@
-import { useState } from 'react';
-import useMousePosition from '../useMousePosition';
+import React from 'react';
+import Draggable from 'react-draggable';
 import './Disc.css';
 
-function Disc(props){
-    const [mouseClick, setMouseClick] = useState(false)
-
-    const mousePosition = useMousePosition()
-    
-    function moveDisc(){
-        console.log(mousePosition)
-    }
-    
-    return(
-        <div onMouseEnter = {moveDisc}>
-            <div id = {props.diskType}>
-                <div id = "inner">
-                    <div id = "inner-inner"></div>
+const Disc = React.forwardRef((props, ref) => (
+    <Draggable
+        bounds={{ top: -50 * window.innerHeight / 100, bottom: 50 * window.innerHeight / 100 }}>
+        <div ref={ref}>
+            <div id={props.diskType}>
+                <div id="inner">
+                    <div id="inner-inner"></div>
                 </div>
             </div>
         </div>
-    )
-}
+    </Draggable>
+));
 
 export default Disc;
--- personal-website-v2/src/sub-components/Music.js
+++ personal-website-v2/src/sub-components/Music.js
@@ -1,19 +1,84 @@
-import Disc from './Disc'
-import './Music.css'
+import React, { useRef, useState } from 'react';
+import useSound from 'use-sound';
+import Disc from './Disc';
+import './Music.css';
+
+import bossMusic from '../audio/bossmusic.mp3';
+import nostalgicMusic from '../audio/nostalgic.mp3';
+import romanticMusic from '../audio/romantic.mp3';
 
 function Music(){
+    const [discPlaying, setDiscPlaying] = useState(false)
+
+    const [ boss ] = useSound(bossMusic, {
+        interrupt: true,
+    });
+
+    const [ nostalgic ] = useSound(nostalgicMusic, {
+        interrupt: true,
+    });
+
+    const [ romance ] = useSound(romanticMusic, {
+        interrupt: true,
+    });
+
+    const greyRef = useRef(null)
+    const discOne = useRef(null)
+    const discTwo = useRef(null)
+    const discThree = useRef(null)
+
+    function checkOverlap(){
+        var rect = greyRef.current.getBoundingClientRect();
+        var discUn = discOne.current.getBoundingClientRect();
+        var discDeux = discTwo.current.getBoundingClientRect();
+        var discTrois = discThree.current.getBoundingClientRect();
+
+        if(discPlaying === false && discUn.top < rect.top + 15 && discUn.bottom > rect.bottom - 15){
+            if (discUn.left > rect.left - 15 && discUn.right < rect.right + 70){
+                alert("one")
+                setDiscPlaying(true)
+            }
+        }
+
+        else if(discPlaying === false && discDeux.top < rect.top + 15 && discDeux.bottom > rect.bottom - 15){
+            if (discDeux.left > rect.left - 15 && discDeux.right < rect.right + 70){
+                alert("two")
+                setDiscPlaying(true)
+            }
+        }
+
+        else if(discPlaying === false && discTrois.top < rect.top + 15 && discTrois.bottom > rect.bottom - 15){
+            if (discTrois.left > rect.left - 15 && discTrois.right < rect.right + 70){
+                alert("three")
+                setDiscPlaying(true)
+            }
+        }
+
+        else {
+            setDiscPlaying(false)
+        }
+    }
+
     return(
         <div id = "cont-two">
             <div id = "cont-one">
                 <div id = "record-player">
-                
+                    <div id = "grey-mid" ref = {greyRef}>
+                        <div id = "mid"></div>
+                    </div>
+                    <div id = "base" onClick={checkOverlap}>
+                        <div id = "stick" onClick={checkOverlap}></div>
+                        <div id = "anchor"></div>
+                    </div>
                 </div>
                 <div id = "cat-dance">
                     
                 </div>
             </div>
             <div id = "disc-choice">
-                <Disc diskType = "disc-one"/>
+                <Disc diskType = "disc-one" ref = {discOne}/>
+                <Disc diskType = "disc-two" ref = {discTwo}/>
+                <Disc diskType = "disc-one" ref = {discThree}/>
             </div>
         </div>
     )
